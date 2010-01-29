15a54ab25cbb342a40cdd765c5ce17f8e0673960
marcopolo1010 <marcopolo1010@gmail.com>
2010-01-29 12:12:55 -0500

    completed scoring functions, best results so far.

--- __init__.py
+++ __init__.py
@@ -6,33 +6,204 @@
 import xlrd_util, score_util
 
 class XlsXtractor(object):
-    self._workbook = None
-    self._cells = None
+    _workbook = None
+    _sheetinfo = {}
     
     def __init__(self, filename):
         self._filename = filename
         self._workbook = xlrd_util.load_file(filename)
+        if self._workbook:
+            self.valid = True
+            self.nsheets = self._workbook.nsheets
+        else:
+            self.valid = False
     
-    def extract_schemas(self):
-        for i in range(self._workbook.nsheets):
-            self._cells = self._parse_sheets()
-            self._compute_row_scores()
-            self._dumb_classify()
-            self._smart_classify()
+    def extract_schemas(self, sheet_id):
+        sheetinfo = {}
+        self._sheetinfo[sheet_id] = sheetinfo
         
-    def output(format='html'):
+        sheetinfo['cellblock'] = self._parse_sheet(sheet_id)
+        self._compute_row_scores(sheet_id)
+        self._dumb_classify(sheet_id)
+        self._smart_classify(sheet_id)
+        
+    def output(self, sheet_id, format='html'):
+        if format == 'text':
+            output_text(sheet_id)
+        else:
+            output_html(sheet_id)
+
+    def output_text(self, sheet_id):
+        sheetinfo = self._sheetinfo[sheet_id]
+        scores = sheetinfo['scores']
+        labels = sheetinfo['labels']
+        print 'ROW  LABEL       META   SIM  HEADER'
+        for row in sheetinfo['cellblock'].row_list:
+            if scores['empty'][row] == 1.0:
+                continue
+            print '%3d: ' % row,
+            print '%8s (' % labels[row],
+            print '%.3f, ' % scores['metadata'][row],
+            print '%.3f, ' % scores['similarity'][row],
+            print '%.3f)' % scores['header'][row]
+                    
+    def output_html(self):
         pass
 
-    def output_html():
-        pass
+    def validate(self, sheet_id, annotated_labels):
+        results = {}
+        labels = self._sheetinfo[sheet_id]['labels']
+        for row in annotated_labels.keys():
+            result = '%s_%s' % (annotated_labels[row], labels[row])
+            results[result] = results.get(result, 0) + 1
+        return results
 
-    def validate(annotated_labels):
-        pass
-
-    def _parse_sheet(sheet_id):
-        """Parses spreadsheet cells and stores results in self.cells.
-        """
+    def _parse_sheet(self, sheet_id):
+        """Parses spreadsheet cells and stores results in self.cells."""
         return xlrd_util.CellBlock(self._workbook, sheet_id)
         
-    def _compute_row_scores():
+    def _compute_row_scores(self, sheet_id):
+        """Computes probability that this row is byline, header, or data."""
+        
+        sheetinfo = self._sheetinfo[sheet_id]
+        
+        cellblock = sheetinfo['cellblock']
+        cells = cellblock.cells
+        row_list = cellblock.row_list
+                
+        empty = {}
+        metadata_scores = {}
+        header_scores = {}
+        similarity_scores = {}
+        header_text = {}
+        
+        # initialize scores
+        for row in cellblock.row_list:
+            empty[row] = xlrd_util.check_for_empty_row(cellblock, row)
+            try:
+                metadata_scores[row] = score_util.calc_metadata_score(cellblock, row)
+            except:
+                print row, cellblock.col_list
+                raise
+            similarity_scores[row] = 0
+            header_scores[row] = 0
+
+        for i, row in enumerate(row_list):
+
+            if empty[row]:
+                continue
+
+            # find next row in row_list that is not empty or metadata
+            next_row = None
+            for n in row_list[i+1:]:
+                if not empty[n] and metadata_scores[n] < 0.6:
+                    next_row = n
+                    break
+            if next_row is None:
+                continue
+                
+            gap = (next_row-row)
+            if gap > 6:
+                continue
+            
+            # calculate similarity and header scores
+            s = score_util.calc_sim_score(cellblock, row, next_row)
+            h = score_util.calc_header_score(cellblock, row, next_row)
+            
+            # penalize if there's a big gap
+            if gap > 2:
+                s -= 0.3
+            elif gap > 4:
+                s -= 0.7
+            
+            similarity_scores[row] = s
+            (header_scores[row], header_text[row]) = h
+        
+        scores = {}        
+        scores['empty'] = empty
+        scores['metadata'] = metadata_scores
+        scores['similarity'] = similarity_scores
+        scores['header'] = header_scores
+        
+        sheetinfo['scores'] = scores
+        
+        sheetinfo['header_text'] = header_text
+
+    def _dumb_classify(self, sheet_id):
+        """Having computed metrics for each row, determine a label for each."""
+
+        sheetinfo = self._sheetinfo[sheet_id]
+        scores = sheetinfo['scores']
+        empty = scores['empty']
+        metadata_scores = scores['metadata']
+        header_scores = scores['header']
+        similarity_scores = scores['similarity']
+        header_text = sheetinfo['header_text']
+
+        labels = {}
+        row_scores = {}
+        
+        in_block = False
+        schema_blocks = []
+        
+        for row in sheetinfo['cellblock'].row_list:
+            # classify rows based on header score, similarity score        
+            row_scores[row] = 0.0
+            labels[row] = 'UNKNOWN'
+            
+            if empty[row]:
+                labels[row] = 'EMPTY'
+            
+            if not in_block:
+                if empty[row]:
+                    continue
+                if (metadata_scores[row] < 0.2 and header_scores[row] < 0.2):
+                    labels[row] = 'METADATA'
+                elif (header_scores[row] > 0.5 and 
+                      header_scores.get(row + 1, 0.0) < header_scores[row]):
+                    labels[row] = 'HEADER'
+                    row_scores[row] = header_scores[row]
+                    in_block = True
+                else:
+                    labels[row] = 'METADATA'
+                    row_scores[row] = metadata_scores[row]
+            else:
+                #schema_score = calculate_schema_score(cells[row], current_block)
+                if empty[row] and empty.get(row + 1, 0):
+                    in_block = False
+                elif header_scores[row] > 1.0 and similarity_scores[row] < .8:
+                    row_scores[row] = header_scores[row]
+                    labels[row] = 'HEADER'
+                    in_block = False
+#                elif len(current_block['data_rows']) > 2: # and schema_score < .25:
+#                    labels[row] = 'METADATA'
+#                    row_scores[row] = metadata_scores[row]
+                elif similarity_scores[row] > .4:
+                    labels[row] = 'DATA'
+                    row_scores[row] = similarity_scores[row]
+#                elif (((row + 1 >= num_rows) or empty[row + 1]) and
+#                      row - 1 > 0 and similarity_scores[row-1] > .4):
+#                    labels[row] = 'DATA'
+#                elif len(current_block['data_rows']) > 2 and schema_score > .8:
+#                    labels[row] = 'DATA'
+            
+            # create new schema if we found a new header
+#            if labels[row] == 'HEADER':
+#                current_block = {}
+#                current_block['cols'] = block['cols']
+#                current_block['header_row'] = row
+#                current_block['data_rows'] = []
+#                current_block['header_text'] = header_text[row]
+#                current_block['data_types'] = [{} for c in current_block['cols']]
+#                schema_blocks.append(current_block)
+#            elif labels[row] == 'DATA':
+#                row_score[row] = schema_score
+#                current_block['data_rows'].append(row)
+#                d = current_block['data_types']
+#                for i, col in enumerate(current_block['cols']):
+#                    increment_dict(d[i],cells[row][col]['type'])
+        sheetinfo['labels'] = labels
+        sheetinfo['scores']['row_scores'] = row_scores
+    
+    def _smart_classify(self, sheet_id):
         pass
--- xlrd_util.py
+++ xlrd_util.py
@@ -1,6 +1,14 @@
-from xlrd import open_workbook
+import re
+from xlrd import open_workbook, XLRDError
 
-def load_file(filename=self._filename):
+CELL_TYPE_LOOKUP = ['EMPTY','TEXT','NUMBER','DATE','BOOLEAN','ERROR','BLANK']
+
+re_fmt = re.compile('[\$0-9#\.\,]')
+re_num = re.compile('[\%#0-9\.\,]')
+re_alpha = re.compile('[A-z]')
+re_year = re.compile('[1-2][0-9][0-9][0-9]')
+
+def load_file(filename):
     """Attempt to open the specified file using xlrd.  
     
     Catch and suppress common exceptions, but output a warning.
@@ -29,28 +37,34 @@ def load_file(filename=self._filename):
     except AssertionError:
         print 'Error in XLS parsing library, file could not be opened.'
         return
-    self._workbook = workbook
+    return workbook
 
 
 class CellBlock(object):
     """Represents a block of cells in an Excel Worksheet."""
-    
-    cells = []
-    
+        
     def __init__(self, workbook, sheet_id):
-        
-        sheet = workbook.sheet_by_idx(sheet_id)
-        
-        get_cell_attributes(workbook, sheet)
-        mark_merged_cells(sheet.merged_cells)
-    
-    def get_cell_attributes(workbook, sheet):
-        
+
         self.cells = []
+        self.row_list = []
+        self.col_list = []
+        
+        sheet = workbook.sheet_by_index(sheet_id)
+        
+        self.get_cell_attributes(workbook, sheet)
+        self.mark_merged_cells(sheet.merged_cells)
+        self.update_cell_lists()
+    
+    def get_cell_attributes(self, workbook, sheet):
+        
+        cells = []
 
         xf_list = workbook.xf_list
         font_list = workbook.font_list
         format_map = workbook.format_map
+        format_map_str = {}
+        for k in format_map:
+            format_map_str[k] = ('%s' % format_map[k].format_str).encode('utf8')
         
         for row_index in range(sheet.nrows):
             cells.append([None]*sheet.ncols)
@@ -59,18 +73,15 @@ class CellBlock(object):
                 c = sheet.cell(row_index, col_index)
                 
                 cell_value = c.value
-                cell_value_str = ('%s' % cell_value).encode('utf8')
+                try:
+                    cell_value_str = str(cell_value)
+                except:
+                    cell_value_str = ('%s' % cell_value).encode('utf8')
+                    
                 cell_type = CELL_TYPE_LOOKUP[c.ctype]
-                
-                cell_xf_idx = c.xf_index
-                cell_xf = xf_list[cell_xf_idx]
-                
-                cell_font_idx = cell_xf.font_index
-                cell_font = font_list[cell_font_idx]
-                
-                cell_format_idx = cell_xf.format_key
-                cell_format = format_map[cell_format_idx]
-                cell_format_str = ('%s' % cell_format.format_str).encode('utf8')
+                cell_xf = xf_list[c.xf_index]
+                cell_font = font_list[cell_xf.font_index]
+                cell_format_str = format_map_str[cell_xf.format_key]
                 
                 cell_style = {}
                 cell_style['bold'] = cell_font.bold
@@ -88,11 +99,14 @@ class CellBlock(object):
                 current_cell['merged'] = 0
                 
                 cells[row_index][col_index] = current_cell
+                
+        self.cells = cells
     
-    def mark_merged_cells(merged_cells):
+    def mark_merged_cells(self, merged_cells):
         """ Mark merged cells, copy value from top-left cell to 
         others in merged area
         """
+        cells = self.cells
         for crange in merged_cells:
             rlo, rhi, clo, chi = crange
             for rowx in xrange(rlo, rhi):
@@ -100,5 +114,53 @@ class CellBlock(object):
                     cells[rowx][colx]['merged'] = 1
                     cells[rowx][colx]['value'] = cells[rlo][clo]['value']
             cells[rlo][clo]['merged'] = (rhi-rlo, chi-clo)
-
         
+
+    def update_cell_lists(self):
+        """Remove empty columns from column list"""
+        self.row_list = range(len(self.cells))
+        if len(self.row_list) == 0:
+            return
+        
+        for col in range(len(self.cells[0])):
+            for row in self.row_list:
+                c = self.cells[row][col]
+                if not check_for_empty_cell(c):
+                    self.col_list.append(col)
+                    break
+
+def check_for_empty_cell(c):
+    if (c['value_str'] == '' or
+        c['type'] in ['BLANK','EMPTY']):
+        return 1
+    else:
+        return 0
+        
+def check_for_empty_row(cellblock, row_index):
+    for col in cellblock.col_list:
+        c = cellblock.cells[row_index][col]
+        if not check_for_empty_cell(c):
+            return 0
+    return 1
+    
+def check_for_numeric_cell(c):
+    score = 0.0
+    
+    if check_for_empty_cell(c):
+        return 1.0
+    
+    if (c['type'] != 'NUMBER' or
+        re_year.match(c['value_str'])):
+        score -= 1
+        
+    # add 2 if we see currencies, percentages, or numbers
+    if (re_fmt.search(c['format_str']) and
+        re_num.search(c['value_str']) and
+        not re_alpha.search(c['value_str'])):
+        score += 2
+    
+    # TODO
+    # add 10 if the cell above and cell below are both numbers
+    # and the three are in a sequence
+    
+    return score
